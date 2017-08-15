6b452027606c196ed47ee262ea733b21c334f797
anmolduainter <anmolduainter@gmail.com>
2017-08-16 00:08:52 +0530

    wensorization

--- Movies.R
+++ Movies.R
@@ -1,5 +1,23 @@
 library(ggplot2movies)
 
-names(movies)
+# name of movies
+names(movies)   
 
+# average length
 mean(movies$length)
+
+#average budget
+mean(movies$budget)
+
+#average budget after removing NA
+mean(movies$budget,na.rm=TRUE)
+
+#frequency of missing budget
+mean(is.na(movies$budget))
+
+# return dataset after removing missing data
+moviesNoNa=na.omit(movies)
+
+# generating plot
+qplot(rating,budget,data=moviesNoNa,size=1.2)+stat_smooth(color="Red",size=2,se=F)
+
